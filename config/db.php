1e1ea47f64fdac987350eec5f31f28953f2cb82b
computer0316 <computer0316@126.com>
2018-01-03 10:37:02 +0800

    first time

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=bdm246008579.my3w.com;dbname=bdm246008579_db',
-    'username' => 'bdm246008579',
-    'password' => '7777lezu',
+    'dsn' => 'mysql:host=localhost;dbname=lezu',
+    'username' => 'root',
+    'password' => 'vorjtmig',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
