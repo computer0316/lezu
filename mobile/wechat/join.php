ab29c2d53a02afb073c452aca4c9ab2f14cea53d
computer0316 <computer0316@126.com>
2018-01-06 14:39:06 +0800

    wechat
    add some wechat menu

---
+++ mobile/wechat/join.php
@@ -0,0 +1,41 @@
+ <!DOCTYPE html>
+    <html lang=zh-cn>
+    <head>
+	    <meta charset=utf-8>
+	    <title>廊坊乐租</title>
+	    <meta name=description content=廊坊乐租房地产经纪有限公司>
+	    <meta name=author content=Roc>
+	    <meta name=copyright content=Roc>
+	    <link rel=shortcut icon href=favicon.ico>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="format-detection" content="telephone=no">
+	    <link rel="stylesheet" href="main.css">
+	    <script>
+		    function changeFrameHeight(){
+		        var ifm= document.getElementById("iframepage");
+		        ifm.height = document.documentElement.clientHeight + 100;
+		    }
+
+		    window.onresize=function(){
+		         changeFrameHeight();
+		    }
+		</script>
+	    </head>
+    <body>
+    	<div class="box">
+    		<img class="img" src="decoration.jpg" />
+    		<p class="title">精致的装修及维护保养</p>
+    		<p class="content">将您的房屋委托给美丽屋，美丽屋将对房屋进行配套装修设计。合同期内，定期对房屋进行维护保养</p>
+    	</div>
+    	<div class="box">
+    		<img class="img" src="butler.jpg" />
+    		<p class="title">一站式托管，安全省心</p>
+    		<p class="content">签约即刻起，您便拥有专属房屋管家，与房屋出租相关的事物，您的管家会竭诚服务</p>
+    	</div>
+		<img class="procedure" src="procedure.png" />
+		<p class="title">　　请填写房屋信息：</p>
+		<iframe id="iframepage" class="iframe" onload="changeFrameHeight()" scrolling=no src="http://weidian.51vfang.com/Web/Publish/saleLeaseOne/type/2.html?comp_id=238366&broker_id="></iframe>
+    </body>
+</html>
---
+++ mobile/wechat/order.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE html>
+    <html lang=zh-cn>
+    <head>
+	    <meta charset=utf-8>
+	    <title>廊坊乐租</title>
+	    <meta name=description content=廊坊乐租房地产经纪有限公司>
+	    <meta name=author content=Roc>
+	    <meta name=copyright content=Roc>
+	    <link rel=shortcut icon href=favicon.ico>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="black">
+		<meta name="format-detection" content="telephone=no">
+	    <link rel="stylesheet" href="main.css">
+	    <script>
+		    function changeFrameHeight(){
+		        var ifm= document.getElementById("iframepage");
+		        ifm.height = document.documentElement.clientHeight + 50;
+		    }
+
+		    window.onresize=function(){
+		         changeFrameHeight();
+		    }
+		</script>
+	    </head>
+    <body>
+    	<div class="box">
+    		<img style="float:left;width:120px; height:90px;margin:15px;" src="sheng.jpg" />
+    		<p class="title">说出您的租房需求</p>
+    		<p class="content">乐租 房管家帮您：<br />
+    			<b>省时、省力、省心、省钱</b></p>
+    	</div>
+		<p class="title">　　请填写租房需求：</p>
+		<iframe id="iframepage" class="iframe" onload="changeFrameHeight()" scrolling=no src="http://weidian.51vfang.com/Web/Publish/rentOne.html?comp_id=238366&broker_id="></iframe>
+    </body>
+</html>
